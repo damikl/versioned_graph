583cd26ef985544e5de15289e04c9231e92611f5
Damian Lipka <damian.lipka@uj.edu.pl>
2014-03-11 01:52:44 +0100

    better tests

--- main.cpp
+++ main.cpp
@@ -48,14 +48,15 @@ struct edge_printer<no_property>{
     }
 };
 
-template<typename Graph>
+
 struct Printer{
+    template<typename Graph>
     static void print_edges(const Graph& graph){
 
         print(graph, edges(graph).first, edges(graph).second);
     }
 private:
-    template<typename iterator>
+    template<typename Graph,typename iterator>
     static void print(const Graph& graph,iterator begin, iterator end){
         for(iterator edge_iter = begin; edge_iter != end; ++edge_iter) {
             int u = source(*edge_iter, graph);
@@ -99,49 +100,43 @@ int main()
     //Now we can initialize our graph using iterators from our above vector
     UndirectedGraph g(edgeVec.begin(), edgeVec.end(), N);
 
-//    std::cout << num_edges(g) << "\n";
-//    g[D].simple_name = "Alpha";
-
-//    std::cout << "HHH " << typeid(g[D]).name() << std::endl;
-    //Ok, we want to see that all our edges are now contained in the graph
-    typedef graph_traits<UndirectedGraph>::edge_iterator edge_iterator;
-
-    //Tried to make this section more clear, instead of using tie, keeping all
-    //the original types so it's more clear what is going on
-    Printer<UndirectedGraph>::print_edges(g);
-
-    std::cout << "Want to add another edge between (A,E)\n";
-    //Want to add another edge between (A,E)?
     add_edge(A, E, g);
-
-    //Print out the edge list again to see that it has been added
-    Printer<UndirectedGraph>::print_edges(g);
 
     UndirectedGraph back = g;
     graph_archive<UndirectedGraph> arch(g);
     arch.commit();
     //Finally lets add a new vertex - remember the verticies are just of type int
     int F = add_vertex(g);
-    std::cout << "new vertex: " << F << "\n";
 
     //Connect our new vertex with an edge to A...
     add_edge(A, F, g);
     remove_edge(B, D, g);
-    std::cout << "removed edge: " << B <<" to " << D << "\n";
+
     g[F].simple_name = "other";
     g[D].simple_name = "just something";
-    Printer<UndirectedGraph>::print_edges(g);
 
     arch.commit();
-    std::cout << "before checkout\n";
     UndirectedGraph ng = arch.checkout(1);
     bool first = equal(back,ng);
+    if(first){
+        std::cout << "versions are equal" << std::endl;
+    } else{
+        std::cout << "versions not equal" << std::endl;
+        Printer::print_edges(back);
+        std::cout << "verus" << std::endl;
+        Printer::print_edges(ng);
+    }
     bool second = equal(arch.checkout(2),g);
-    std::cout << "Comparison: " << first << " " << second << std::endl;
+    if(second){
+        std::cout << "versions are equal" << std::endl;
+    } else{
+        std::cout << "versions not equal" << std::endl;
+        Printer::print_edges(g);
+        std::cout << "verus" << std::endl;
+        Printer::print_edges(arch.checkout(2));
+    }
 
-    Printer<UndirectedGraph>::print_edges(g);
-
-    cout << "Graph with no extra data" << endl;
+    cout << endl << "Graph with no extra data" << endl;
 
     SimpleGraph simple(edgeVec.begin(), edgeVec.end(), N);
     SimpleGraph s_copy = simple;
@@ -152,11 +147,25 @@ int main()
     simple_arch.commit();
 
     first = equal(s_copy,simple_arch.checkout(1));
+    if(first){
+        std::cout << "versions are equal" << std::endl;
+    } else{
+        std::cout << "versions not equal" << std::endl;
+        Printer::print_edges(s_copy);
+        std::cout << "verus" << std::endl;
+        Printer::print_edges(simple_arch.checkout(1));
+    }
     second = equal(simple_arch.checkout(2),simple);
-    std::cout << "Comparison: " << first << " " << second << std::endl;
-    Printer<SimpleGraph>::print_edges(simple);
+    if(second){
+        std::cout << "versions are equal" << std::endl;
+    } else{
+        std::cout << "versions not equal" << std::endl;
+        Printer::print_edges(simple);
+        std::cout << "verus" << std::endl;
+        Printer::print_edges(simple_arch.checkout(2));
+    }
 
-    cout << "Graph with data on vertices and edges" << endl;
+    cout << endl << "Graph with data on vertices and edges" << endl;
 
     ExtendedGraph ex(edgeVec.begin(), edgeVec.end(), N);
     ExtendedGraph ex_copy = ex;
@@ -171,33 +180,24 @@ int main()
 //    ex[A].simple_name = "aaa";     // breaks next comparison
 
     first = equal(ex_copy,ex_arch.checkout(1));
+    if(first){
+        std::cout << "versions are equal" << std::endl;
+    } else{
+        std::cout << "versions not equal" << std::endl;
+        Printer::print_edges(ex_copy);
+        std::cout << "verus" << std::endl;
+        Printer::print_edges(ex_arch.checkout(1));
+    }
     second = equal(ex_arch.checkout(2),ex);
-    std::cout << "Comparison: " << first << " " << second << std::endl;
-    Printer<ExtendedGraph>::print_edges(ex);
+    if(second){
+        std::cout << "versions are equal" << std::endl;
+    } else{
+        std::cout << "versions not equal" << std::endl;
+        Printer::print_edges(ex_arch.checkout(2));
+        std::cout << "verus" << std::endl;
+        Printer::print_edges(ex);
+    }
 
- /*
-    typedef property<edge_weight_t, int> EdgeWeightProperty;
-    typedef boost::adjacency_list<listS, vecS, directedS, no_property,EdgeWeightProperty > mygraph;
-
-
-    mygraph w;
-    add_edge(0, 1, 8, w);
-    add_edge(0, 3, 18, w);
-    add_edge(1, 2, 20, w);
-    add_edge(2, 3, 2, w);
-    add_edge(3, 1, 1, w);
-    add_edge(1, 3, 7, w);
-    mygraph w_copy = w;
-    graph_archive<mygraph> w_arch(w);
-    w_arch.commit();
-    add_edge(3, 2, 2, w);
-    w_arch.commit();
-
-    first = equal(w_copy,w_arch.checkout(1));
-    second = equal(w_arch.checkout(2),w);
-    std::cout << "Comparison: " << first << " " << second << std::endl;
-    Printer<mygraph>::print_edges(w);
-*/
     return 0;
 }
 
