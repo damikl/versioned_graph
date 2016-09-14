22094c8d72ad5ad73c5c7c7d3df6899aa8f584b1
Damian Lipka <damikl1989@gmail.com>
2016-09-14 04:24:16 +0200

    fixed example

--- example.cpp
+++ example.cpp
@@ -1,51 +1,99 @@
 #include "versioned_graph.h"
+using namespace boost;
+using namespace std;
+typedef versioned_graph<adjacency_list<boost::listS, boost::listS, boost::directedS, int, int, string>> simple_graph;
+typedef typename boost::graph_traits<simple_graph>::vertex_descriptor vertex_descriptor;
+
+vertex_descriptor a,b,c,d,e,f;
+
+void create_state1(simple_graph& sg){
+    a = add_vertex(1,sg);
+    b = add_vertex(2,sg);
+    c = add_vertex(3,sg);
+    add_edge(a,b,7,sg);
+    add_edge(a,c,6,sg);
+    sg[graph_bundle] = "10%";
+}
+void verify_state1(const simple_graph& sg){
+    assert(edge(a,b,sg).second);
+    assert(edge(a,c,sg).second);
+    assert(num_edges(sg)==2);
+    assert(num_vertices(sg)==3);
+    assert(sg[edge(a,b,sg).first]==7);
+    assert(sg[edge(a,c,sg).first]==6);
+    assert(sg[graph_bundle]=="10%");
+}
+
+void create_state2(simple_graph& sg){
+    remove_edge(edge(a,b,sg).first,sg);
+    remove_vertex(b,sg);
+    d = add_vertex(4,sg);
+    e = add_vertex(5,sg);
+    add_edge(d,e,2,sg);
+    add_edge(e,d,3,sg);
+    add_edge(c,c,7,sg);
+    sg[graph_bundle] = "30%";
+}
+
+void verify_state2(const simple_graph& sg){
+    assert(edge(d,e,sg).second);
+    assert(edge(e,d,sg).second);
+    assert(!edge(a,b,sg).second);
+    assert(num_edges(sg)==4);
+    assert(num_vertices(sg)==4);
+    assert(sg[edge(c,c,sg).first]==7);
+    assert(sg[graph_bundle]=="30%");
+}
+
+void create_state3(simple_graph& sg){
+    clear_vertex(e,sg);
+    remove_vertex(e,sg);
+    f = add_vertex(6,sg);
+    sg[edge(c,c,sg).first] = 9;
+    add_edge(d,f,8,sg);
+    sg[a] = 4; sg[c] = 5;
+    sg[graph_bundle] = "60%";
+}
+
+void verify_state3(const simple_graph& sg){
+    assert(num_edges(sg)==3);
+    assert(num_vertices(sg)==4);
+}
+
+void create_state4(simple_graph& sg){
+    sg[edge(a,c,sg).first] = 1;
+    add_edge(c,f,3,sg);
+}
 
 int main(){
-    using namespace boost;
-    using namespace std;
-    typedef versioned_graph<adjacency_list<boost::vecS, boost::listS, boost::directedS,int,string>> simple_graph;
-    simple_graph sg;
-    typedef typename boost::graph_traits<simple_graph>::vertex_descriptor vertex_descriptor;
-    vertex_descriptor A = add_vertex(1,sg);
-    vertex_descriptor B = add_vertex(2,sg);
-    vertex_descriptor C = add_vertex(3,sg);
-    vertex_descriptor D = add_vertex(6,sg);
-    add_edge(A,B,std::string("9"),sg);
-    add_edge(A,C,"8",sg);
-    add_edge(B,D,"7",sg);
-    add_edge(A,D,"11",sg);
-    add_edge(B,C,"12",sg);
-    sg[D] = 4;
-    assert(4==num_vertices(sg));
-    assert(5==num_edges(sg));
-    cout << "start commit" << endl;
-    commit(sg);
-    cout << "commit end" << endl;
-    sg[D] = 5;
-    remove_edge(A,D,sg);
-    assert(4==num_edges(sg));
-    assert(!edge(A,D,sg).second);
-    commit(sg);
-    sg[D] = 6;
-    undo_commit(sg);
-    cout << "made undo" << endl;
-    assert(edge(A,D,sg).second);
-    cout << "edge recreated" << endl;
-    assert(5==num_edges(sg));
-    cout << "count match" << endl;
-    assert(4==sg[D]);
-    cout << "attribute match" << endl;
-    vertex_descriptor E = add_vertex(5,sg);
-    add_edge(E,D,"13",sg);
-    add_edge(C,E,"14",sg);
-    assert(5==num_vertices(sg));
-    assert(7==num_edges(sg));
-    assert(5==sg[E]);
-    revert_changes(sg);
-    assert(4==num_vertices(sg));
-    assert(5==num_edges(sg));
 
-    typedef versioned_graph<adjacency_list<boost::hash_setS, boost::hash_setS, boost::directedS,int,string>> hash_graph;
-    hash_graph g;
-    //typedef typename boost::graph_traits<hash_graph>::vertex_descriptor vertex_descr;
+    simple_graph sg;
+
+    create_state1(sg);
+    verify_state1(sg);
+    commit(sg);		// stan 1
+
+
+    create_state2(sg);
+    verify_state2(sg);
+    // revert_changes(sg); verify_state1(sg);
+    commit(sg); // stan 2
+
+
+    create_state3(sg);
+    // revert_changes(sg); verify_state2(sg);
+    // undo_commit(sg); verify_state1(sg);
+    commit(sg); // stan 3
+    verify_state3(sg);
+    // undo_commit(sg);  verify_state2(sg);
+
+    create_state4(sg);
+    revert_changes(sg); // stan 3
+    verify_state3(sg);
+    undo_commit(sg);
+    verify_state2(sg);
+    undo_commit(sg);
+    verify_state1(sg);
+
 }
+
